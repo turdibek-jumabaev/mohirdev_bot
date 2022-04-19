b836442fd8ae779a652a5d4363aede69c1d671d0
Turdibek <turdibekdev05@gmail.com>
2022-04-19 23:58:16 +0500

    handler yangilandi

--- handlers/users/main_handler.py
+++ handlers/users/main_handler.py
@@ -1,5 +1,5 @@
 from loader import dp
-from data.matnlar import ustozlar
+from data.matnlar import ustozlar, kurslar
 from states.main_state import MainMenuState
 from keyboards.default.ustozlar_btn import USTOZLAR_BUTTON
 
@@ -9,7 +9,7 @@ from aiogram import types
 @dp.message_handler(content_types=types.ContentType.TEXT)
 async def echo(message: types.Message):
     if message.text == 'Kurslar':
-        await message.answer('Kurslar')
+        await message.answer(kurslar())
 
     elif message.text == 'Practicum':
         await message.answer('Practicum')
